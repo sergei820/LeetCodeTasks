7c0234987c666664a705e273e137dcfc273fd52b
serje <serje@mayflower.work>
2024-10-09 00:33:41 +0300

    Task 125 - Accepted Good solution Runtime 47ms Beats 45.13%

--- easy/125_valid_palindrome.py
+++ easy/125_valid_palindrome.py
@@ -17,9 +17,30 @@ class Solution:
 
         return result == reversed_result
 
+class Solution2:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        while right > left:
+            if not s[right].isalnum():
+                right -= 1
+                continue
+
+            if not s[left].isalnum():
+                left += 1
+                continue
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            right -= 1
+            left += 1
+
+        return True
+
 
 if __name__ == '__main__':
-    s = Solution()
+    s = Solution2()
     print(s.isPalindrome("A man, a plan, a canal: Panama"))
     print(s.isPalindrome("raceacar"))
     print(s.isPalindrome("0P"))
