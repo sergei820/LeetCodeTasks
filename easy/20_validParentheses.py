ac0fee5310b32fb5b7e9175d588e55e6a8a359dc
serje <serje@mayflower.work>
2024-11-06 17:14:09 +0200

    205 - Done, Runtime 0ms Beats 100.00%, Memory 16.50MB Beats 96.69%

---
+++ easy/20_validParentheses.py
@@ -0,0 +1,58 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for bracket in s:
+            if len(stack) == 0 and bracket in ')}]':
+                return False
+            if len(stack) == 0:
+                stack.append(bracket)
+                continue
+
+            if bracket == ')' and stack[-1] == '(':
+                stack.pop()
+            elif bracket == ']' and stack[-1] == '[':
+                stack.pop()
+            elif bracket == '}' and stack[-1] == '{':
+                stack.pop()
+            else:
+                stack.append(bracket)
+
+        return len(stack) == 0
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.isValid("()"))
+    print(s.isValid("()[]{}"))
+    print(s.isValid("(]"))  # false
+    print(s.isValid("([])"))
+
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+# determine if the input string is valid.
+#
+# An input string is valid if:
+#
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+#
+# Example 1:
+# Input: s = "()"
+# Output: true
+#
+# Example 2:
+# Input: s = "()[]{}"
+# Output: true
+#
+# Example 3:
+# Input: s = "(]"
+# Output: false
+#
+# Example 4:
+# Input: s = "([])"
+# Output: true
+#
+# Constraints:
+# 1 <= s.length <= 104
+# s consists of parentheses only '()[]{}'.
